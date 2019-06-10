ebce853f72ecb7827b87fd20e31af21624231796
kakiang <46804492+kakiang@users.noreply.github.com>
2019-06-10 14:33:16 +0000

    Add NavigationControls to webview
    Signed-off-by: kakiang <46804492+kakiang@users.noreply.github.com>

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,9 +1,8 @@
-// import 'dart:async';
-
 import 'dart:async';
 
 import 'package:briefing/model/article.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleWebView extends StatefulWidget {
@@ -43,22 +42,82 @@ class _ArticleWebViewState extends State<ArticleWebView> {
     //   ),
     // );
 
-    return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.light,
-        iconTheme: IconThemeData(),
-        elevation: 1.0,
-        title: Text(
-          widget.article.channel.title,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark.copyWith(
+          statusBarColor: Colors.white,
+          systemNavigationBarColor: Colors.white,
+          systemNavigationBarIconBrightness: Brightness.dark),
+      child: Scaffold(
+        appBar: AppBar(
+          brightness: Brightness.light,
+          iconTheme: IconThemeData(),
+          elevation: 1.0,
+          title: Text(
+            widget.article.channel.title,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          actions: <Widget>[
+            NavigationControls(_controller.future),
+          ],
+        ),
+        body: WebView(
+          initialUrl: widget.article.link,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller) {
+            _controller.complete(controller);
+          },
         ),
       ),
-      body: WebView(
-        initialUrl: widget.article.link,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController controller) {
-          _controller.complete(controller);
-        },
-      ),
     );
+  }
+}
+
+class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: true),
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: false),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  navigate(BuildContext context, WebViewController controller,
+      {bool goBack: false}) async {
+    bool canNavigate =
+        goBack ? await controller.canGoBack() : await controller.canGoForward();
+    if (canNavigate) {
+      goBack ? controller.goBack() : controller.goForward();
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+            content: Text("No ${goBack ? 'back' : 'forward'} history item")),
+      );
+    }
   }
 }
